74863f8b02b18f1722854b788b63516d95f89621
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2021-11-18 17:01:46 -0300

    Creating basic user and plan models

--- Trabalho2/SocioTorcedor/models.py
+++ Trabalho2/SocioTorcedor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    name = models.CharField(max_length=40)
+    cpf = models.IntegerField(max_length=11, unique=True)
+
+class Plano(models.Model):
+    nome = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2)
