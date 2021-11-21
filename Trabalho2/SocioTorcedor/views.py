ea7346f6d39df302ede9678cb6ca12a85c670848
Tomás Cavalcante Valadão <valadaotomas@gmail.com>
2021-11-21 18:23:52 -0300

    [Trabalho 2] - LoginRequired

--- Trabalho2/SocioTorcedor/views.py
+++ Trabalho2/SocioTorcedor/views.py
@@ -1,16 +1,25 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic.base import View
+from django.views.generic.edit import UpdateView
 from .models import Plano
 from .forms import UsuarioForm
 
-class PlansView(View):
+class OverrideUpdateView(LoginRequiredMixin, UpdateView):
+    def get(self, request, pk, *args, **kwargs):
+        if request.user.id == pk:
+            return super().get(request, pk, args, kwargs)
+        else:
+            return redirect('sec-home')
+
+class PlansView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         plans = Plano.objects.all()
 
         return render(request, 'planos.html', {'plans': plans})
 
-class UserView(View):
+class UserView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         context = {'formulario' : UsuarioForm}
         return render(request, 'cadastro.html', context)
