1aaee0a82464aceac9ea669d0ad7bf7135363aad
Tomás Cavalcante Valadão <valadaotomas@gmail.com>
2021-11-28 21:38:23 -0300

    [Trabalho 2] - Deploy

--- Trabalho2/Trabalho2/settings.py
+++ Trabalho2/Trabalho2/settings.py
@@ -115,11 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
- os.path.join(BASE_DIR, 'static'),
-]
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
